7297a2b92b9d6d04873a9b41c1da4581b3325788
dei79 <dirk.eisenberg@gmail.com>
2012-11-14 21:00:18 +0100

    Block support

--- lib/jquery/modal/helpers/link_helpers.rb
+++ lib/jquery/modal/helpers/link_helpers.rb
@@ -9,25 +9,37 @@ module Jquery
     #   link_to(body, url, html_options = {})
     #   link_to(body, url)
     #
-    def link_to_modal(body, url, html_options = {})
-      # extend the html_options
-      html_options[:rel] = "modal:open"
-      if (html_options.has_key?(:remote))
-        if (html_options[:remote] == true)
-          html_options[:rel] = "modal:open:ajaxpost"
+    def link_to_modal(*args, &block)
+      if block_given?
+        options      = args.first || {}
+        html_options = args.second
+        block_result = yield(*args)
+        link_to_modal(block_result, options, html_options)
+      else
+        name         = args[0]
+        options      = args[1] || {}
+        html_options = args[2] || {}
+
+        # extend the html_options
+        html_options[:rel] = "modal:open"
+        if (html_options.has_key?(:remote))
+          if (html_options[:remote] == true)
+            html_options[:rel] = "modal:open:ajaxpost"
+          end
+
+          # remove the remote tag
+          html_options.delete(:remote)
         end
 
-        # remove the remote tag
-        html_options.delete(:remote)
+        # check if we have an id
+        html_options[:id] = UUIDTools::UUID.random_create().to_s unless html_options.has_key?(:id)
+
+        # perform the normal link_to operation
+        html_link = link_to(name, options, html_options)
+
+        # emit both
+        html_link.html_safe
       end
-      # check if we have an id
-      html_options[:id] = UUIDTools::UUID.random_create().to_s unless html_options.has_key?(:id)
-
-      # perform the normal link_to operation
-      html_link = link_to(body, url, html_options)
-
-      # emit both
-      html_link.html_safe
     end
   end
 end
