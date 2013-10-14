881ed420b3f4cb5902006c422c667ae03820ef80
Lucas Clemente <lucas@clemente.io>
2013-10-14 21:05:17 +0200

    update jquery-modal to 0.5.4

--- vendor/assets/javascripts/jquery.modal.js
+++ vendor/assets/javascripts/jquery.modal.js
@@ -1,6 +1,6 @@
 /*
     A simple jQuery modal (http://github.com/kylefox/jquery-modal)
-    Version 0.5.3
+    Version 0.5.4
 */
 (function($) {
 
@@ -11,6 +11,7 @@
     var remove, target;
     this.$body = $('body');
     this.options = $.extend({}, $.modal.defaults, options);
+    this.options.doFade = !isNaN(parseInt(this.options.fadeDuration, 10));
     if (el.is('a')) {
       target = el.attr('href');
       //Select element by id from href
@@ -48,8 +49,16 @@
     constructor: $.modal,
 
     open: function() {
-      this.block();
-      this.show();
+      var m = this;
+      if(this.options.doFade) {
+        this.block();
+        setTimeout(function() {
+          m.show();
+        }, this.options.fadeDuration * this.options.fadeDelay);
+      } else {
+        this.block();
+        this.show();
+      }
       if (this.options.escapeClose) {
         $(document).on('keydown.modal', function(event) {
           if (event.which == 27) $.modal.close();
@@ -65,6 +74,7 @@
     },
 
     block: function() {
+      var initialOpacity = this.options.doFade ? 0 : this.options.opacity;
       this.$elm.trigger($.modal.BEFORE_BLOCK, [this._ctx()]);
       this.blocker = $('<div class="jquery-modal blocker"></div>').css({
         top: 0, right: 0, bottom: 0, left: 0,
@@ -72,14 +82,23 @@
         position: "fixed",
         zIndex: this.options.zIndex,
         background: this.options.overlay,
-        opacity: this.options.opacity
+        opacity: initialOpacity
       });
       this.$body.append(this.blocker);
+      if(this.options.doFade) {
+        this.blocker.animate({opacity: this.options.opacity}, this.options.fadeDuration);
+      }
       this.$elm.trigger($.modal.BLOCK, [this._ctx()]);
     },
 
     unblock: function() {
-      this.blocker.remove();
+      if(this.options.doFade) {
+        this.blocker.fadeOut(this.options.fadeDuration, function() {
+          this.remove();
+        });
+      } else {
+        this.blocker.remove();
+      }
     },
 
     show: function() {
@@ -90,13 +109,24 @@
       }
       this.$elm.addClass(this.options.modalClass + ' current');
       this.center();
-      this.$elm.show().trigger($.modal.OPEN, [this._ctx()]);
+      if(this.options.doFade) {
+        this.$elm.fadeIn(this.options.fadeDuration);
+      } else {
+        this.$elm.show();
+      }
+      this.$elm.trigger($.modal.OPEN, [this._ctx()]);
     },
 
     hide: function() {
       this.$elm.trigger($.modal.BEFORE_CLOSE, [this._ctx()]);
       if (this.closeButton) this.closeButton.remove();
-      this.$elm.removeClass('current').hide();
+      this.$elm.removeClass('current');
+
+      if(this.options.doFade) {
+        this.$elm.fadeOut(this.options.fadeDuration);
+      } else {
+        this.$elm.hide();
+      }
       this.$elm.trigger($.modal.CLOSE, [this._ctx()]);
     },
 
@@ -156,7 +186,9 @@
     modalClass: "modal",
     spinnerHtml: null,
     showSpinner: true,
-    showClose: true
+    showClose: true,
+    fadeDuration: null,   // Number of milliseconds the fade animation takes.
+    fadeDelay: 1.0        // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
   };
 
   // Event constants
