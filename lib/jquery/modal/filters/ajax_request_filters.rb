b59f0ae16bcff2a094d9cbadbb16b34fa64b7871
debian-dev <debian@DESKTOP-VFNGIR9.localdomain>
2021-03-31 11:46:08 -0600

    Change before_filter to before_action

--- lib/jquery/modal/filters/ajax_request_filters.rb
+++ lib/jquery/modal/filters/ajax_request_filters.rb
@@ -7,7 +7,7 @@ module Jquery
       # This filter disables the layout for all AJAX requests so a formular in the dialog
       # can be presentend in the layout of the master form
       #
-      base.send :before_filter do |controller|
+      base.send :before_action do |controller|
         (controller.action_has_layout = false) if controller.request.xhr?
       end
     end
